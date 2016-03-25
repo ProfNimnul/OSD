a75351a64b71d61fcca6e6d9578ff33fdccbd418
Stanislav Solomonov <stanso@ukr.net>
2016-03-25 15:57:53 +0200

    Разобраться с префиксом!!!

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,5 +1,8 @@
 import os
 from easygui import diropenbox
+from os import path
+import pprint
+
 
 dirname = diropenbox(msg="Выберите папку с файлами")
 if not dirname:
@@ -7,14 +10,20 @@ if not dirname:
 
 
 listdir = os.listdir(dirname)
-
+fileparams = dict() # тут хранятся данные о файле - будет словарь словарей
+# на один префикс хранится имя файла, его длина, тип (файл, папка)
 files=dict()
 
 for f in listdir:
-    prefix = f.split("{")[0]
-    if files.get(prefix)==None:
-        files.update({prefix:f})
-        continue
     fullpath=dirname+"\\"+f
-    os.remove(fullpath)
+    prefix = (f.split("}"))[0].split("{")[1]
+   
+    if not prefix in files:
+        files.update({"prefix":prefix,"filename":f,"filelenght":os.path.getsize(fullpath)})
+        
+        pprint.pprint(files)
+        continue
+  
+    
+ #   os.remove(fullpath)
 pass
